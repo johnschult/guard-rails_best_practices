0199b49f2ba94e970f3a49c22a7780689cd96631
John Schult <john@schult.us>
2013-03-08 13:05:17 -0800

    Update rails_best_practices.rb

--- lib/guard/rails_best_practices.rb
+++ lib/guard/rails_best_practices.rb
@@ -10,12 +10,6 @@ module Guard
 
     def initialize(watchers = [], options = {})
       super
-      options[:vendor]   = true   if options[:vendor].nil?
-      options[:spec]     = true   if options[:spec].nil?
-      options[:test]     = true   if options[:test].nil?
-      options[:features] = true   if options[:features].nil?
-      options[:exclude]  = ''     if options[:exclude].nil?
-
       options[:run_at_start] = true if options[:run_at_start].nil?
     end
 
@@ -43,22 +37,16 @@ module Guard
     end
 
     def notify?
-      !!options[:notify]
+      !!options[:notification]
     end
 
   private
     def run_bestpractices
       UI.info 'Running Rails Best Practices checklist', :reset => true
       started_at = Time.now
-
       cmd = 'rails_best_practices'
-      cmd += ' --vendor'   if options[:vendor]
-      cmd += ' --spec'     if options[:spec]
-      cmd += ' --test'     if options[:test]
-      cmd += ' --features' if options[:features]
-      cmd += " --exclude #{options[:exclude]}" unless options[:exclude].blank?
+      cmd += options[:cli] if options[:cli]
       @result = system(cmd)
-
       Notifier::notify( @result, Time.now - started_at ) if notify?
       @result
     end
